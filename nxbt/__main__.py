47eeb58f70f136646bbbb53e5483f9fd49165900
coyote <chenyutao0706@gmail.com>
2026-03-18 09:06:21 +0800

    Disable the guard for fork bombs
    See: https://github.com/Nuitka/Nuitka/blob/1ad18f33f9a763603a8085c70aa21b3c7be0b4a7/README.rst?plain=1#L1067-L1077

--- nxbt/__main__.py
+++ nxbt/__main__.py
@@ -7,6 +7,7 @@
 # nuitka-project: --file-version=0.1.0
 # nuitka-project: --product-version=0.1.0
 # nuitka-project: --output-dir=release
+# nuitka-project: --no-deployment-flag=self-execution
 # nuitka-project: --include-data-dir=./nxbt/web/static=nxbt/web/static
 # nuitka-project: --include-data-dir=./nxbt/web/templates=nxbt/web/templates
 # nuitka-project: --include-data-dir=./nxbt/controller/sdp=nxbt/controller/sdp
