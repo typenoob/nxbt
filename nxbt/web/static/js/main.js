a909644dd86fc0a6db6dd6b4ba57ea0af766b084
coyote <chenyutao0706@gmail.com>
2026-05-25 16:35:47 +0800

    Fix: fix recreateProController

--- nxbt/web/static/js/main.js
+++ nxbt/web/static/js/main.js
@@ -396,7 +396,7 @@ function shutdownController() {
 }
 
 function recreateProController() {
-    socket.emit('create_pro_controller');
+    socket.emit('web_create_pro_controller');
 }
 
 function restartController() {
