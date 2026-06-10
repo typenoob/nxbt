df06ae3d4a2e03c10325a00562e2e4cba0d68d01
coyote <chenyutao0706@gmail.com>
2026-06-10 16:51:18 +0800

    Chores: Finish release preparation

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 from setuptools import setup, Extension
 from Cython.Build import cythonize
 import shutil
+import platform
 
 if shutil.which("ccache") is not None:
     import os
@@ -9,6 +10,13 @@ if shutil.which("ccache") is not None:
 else:
     print("Warning: without ccache reprepare nxbt would be much slower")
 
+extra_compile_args = []
+extra_link_args = []
+if platform.machine() in ("armv7", "armv7l"):
+    # Resolve `Error: conditional branch out of range`
+    extra_compile_args = ["-marm"]
+    extra_link_args = ["-marm"]
+
 setup(
     name="lib",
     ext_modules=cythonize(
@@ -16,10 +24,14 @@ setup(
             Extension(
                 "lib.bumble.hci",
                 sources=["lib/bumble/hci.pyx"],
+                extra_compile_args=extra_compile_args,
+                extra_link_args=extra_link_args,
             ),
             Extension(
                 "lib.bumble.l2cap",
                 sources=["lib/bumble/l2cap.pyx"],
+                extra_compile_args=extra_compile_args,
+                extra_link_args=extra_link_args,
             ),
         ]
     ),
